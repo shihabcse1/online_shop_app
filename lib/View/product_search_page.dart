a1f4b0352d940cb6d781451048de3836d7231b4a
shihabcse1 <tomdshihab@gmail.com>
2022-09-26 09:36:07 +0600

    search field of Product search page is  added

--- lib/View/product_search_page.dart
+++ lib/View/product_search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:online_shop_app/Resources/colors.dart';
 
 class ProductSearchPage extends StatefulWidget {
   const ProductSearchPage({Key? key}) : super(key: key);
@@ -10,6 +11,33 @@ class ProductSearchPage extends StatefulWidget {
 class _ProductSearchPageState extends State<ProductSearchPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
+      appBar: AppBar(
+        backgroundColor: AppColors.backgroundColor,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                fillColor: AppColors.whiteColor,
+                filled: true,
+                contentPadding:
+                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15),
+                hintText: "কাঙ্ক্ষিত পণ্যটি খুঁজুন",
+                suffixIcon: const Icon(Icons.search_outlined),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
