d0e32ce78f0d81ff33401bdea274b687aa1461d1
shihabcse1 <tomdshihab@gmail.com>
2022-09-26 08:32:10 +0600

    first commit

---
+++ lib/View/product_search_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductSearchPage extends StatefulWidget {
+  const ProductSearchPage({Key? key}) : super(key: key);
+
+  @override
+  State<ProductSearchPage> createState() => _ProductSearchPageState();
+}
+
+class _ProductSearchPageState extends State<ProductSearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
